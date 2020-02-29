293cc902768d174814fa08ec25656819098f5503
MBfilipp <filippstryukov@gmail.com>
2020-02-29 14:02:58 +0200

    Add materialize || Add materialize-css card

--- quotes/src/App.js
+++ quotes/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import 'materialize-css';
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div class="row">
+      <div class="col s12 m4">
+        <div class="card blue-grey darken-1">
+          <div class="card-content white-text">
+            <span class="card-title">Цитата</span>
+            <p>I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively.</p>
+          </div>
+          <div class="card-action">
+            <a class="waves-effect waves-light btn">button</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
