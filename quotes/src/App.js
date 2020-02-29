5cfcf92fbf00ac83529a3351efd976f598e1f174
MBfilipp <filippstryukov@gmail.com>
2020-02-29 16:19:56 +0200

    Add styles || Add fetch logic

--- quotes/src/App.js
+++ quotes/src/App.js
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'materialize-css';
+import { randomColor } from './randomColor';
 
 function App() {
+  const [quotes, setQuotes] = useState("Здесь должна быть цитата");
+  const [color, setColor] = useState("#000");
+
+  const generateQuotes = async (event) => {
+    event.preventDefault();
+
+    try {
+
+      const data = await fetch("https://fish-text.ru/get?format=json&number=1&type=title", 
+        {
+          method: "GET",
+        }
+      );
+
+      const dataJson = await data.json();
+
+      setQuotes(dataJson.text);
+      setColor(randomColor());
+
+    } catch(e) {
+      console.log(e.message);
+    }
+    
+  }
   return (
-    <div class="row">
-      <div class="col s12 m4">
-        <div class="card blue-grey darken-1">
-          <div class="card-content white-text">
-            <span class="card-title">Цитата</span>
-            <p>I am a very simple card. I am good at containing small bits of information.
-            I am convenient because I require little markup to use effectively.</p>
-          </div>
-          <div class="card-action">
-            <a class="waves-effect waves-light btn">button</a>
+    <div className="container">
+      <style>{`body { background-color: ${color}; transition: 1s;`}</style>
+      <div className="row center">
+        <div className="col s10 m6 offset-m3 offset-s1" style={{ marginTop: 23 + "%", transition: 1 + "s" }}>
+          <div className="card lighten-5">
+            <div className="card-content white-text">
+              <span className="card-title" style={{ color }}>Цитата</span>
+              <p className="quotes-p" style={{ color }}><span className="mini-icon">&#9998;</span>{quotes}</p>
+            </div>
+            <div className="card-action">
+              <a onClick={generateQuotes} className="waves-effect btn btn-size" style={{ backgroundColor: color }}>Генерировать</a>
+            </div>
           </div>
         </div>
       </div>
     </div>
+    
   );
 }
 
